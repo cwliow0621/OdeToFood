e0b689413120cabb9b9b9f9059489077e163f9e7
ken88ling <ken88ling@gmail.com>
2016-08-22 23:51:01 +1000

    Add a Configuration Source
    add appsettings.json to startup.cs constructor

--- src/OdeToFood/Startup.cs
+++ src/OdeToFood/Startup.cs
@@ -7,11 +7,24 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using System.IO;
 
 namespace OdeToFood
 {
     public class Startup
     {
+        public Startup()
+        {
+            var builder = new ConfigurationBuilder();
+                builder.SetBasePath(Directory.GetCurrentDirectory());
+                            builder.AddJsonFile("appsettings.json");
+            Configuration = builder.Build();
+                             
+        }
+
+        public IConfiguration Configuration { get; set; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -30,7 +43,8 @@ namespace OdeToFood
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                var greeting = Configuration["greeting"];
+                await context.Response.WriteAsync(greeting);
             });
         }
     }
