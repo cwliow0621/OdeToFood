4e587c188fc50bc7bdf40f53b96d52e3a64e5bb5
ken88ling <ken88ling@gmail.com>
2016-08-24 22:58:48 +1000

    ViewModel customize and inject greeting message
    - rename folder models to Entities
    - inject greeting message function to controller

---
+++ src/OdeToFood/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using OdeToFood.Entities;
+using System.Collections.Generic;
+
+namespace OdeToFood.ViewModels
+{
+    public class HomePageViewModel
+    {
+        public IEnumerable<Restaurant> Restaurant { get; set; }
+        public string CurrentGreeting { get; set; }
+
+    }
+}
