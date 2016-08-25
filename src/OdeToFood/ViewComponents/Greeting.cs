5c40a890791dfdaa4588fad7e5f5ebd23bc6483e
ken88ling <ken88ling@gmail.com>
2016-08-26 00:10:54 +1000

    view component
    Add -View component invoke

---
+++ src/OdeToFood/ViewComponents/Greeting.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OdeToFood.Services;
+
+namespace OdeToFood.ViewComponents
+{
+    public class Greeting : ViewComponent
+    {
+        private IGreeter _greeter;
+
+        public Greeting(IGreeter greeter)
+        {
+            _greeter = greeter;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var model = _greeter.GetGreeting();
+            return View("Default",model);
+        }
+    }
+}
